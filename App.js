67a6c7ae4b9a004c5e81fc6c9da4f31b1983ad0f
Justice Velasco <justicevgomez001@gmail.com>
2021-12-03 19:53:46 +0100

    results created

--- App.js
+++ App.js
@@ -25,17 +25,108 @@ export default function App() {
     });
 
     setCurrentTurn(currentTurn == "o" ? "x" : "o");
+
+    checkWinningState();
+  };
+
+  const checkWinningState = () => {
+    //Row result
+    for (let i = 0; i < 3; i++) {
+      const isRowOWinning = gameMap[i].every((cell) => cell == "o");
+      const isRowXWinning = gameMap[i].every((cell) => cell == "x");
+      if (isRowOWinning) {
+        gameWon("o");
+      }
+      if (isRowXWinning) {
+        gameWon("x");
+      }
+    }
+    //End Row result
+
+    //Column result
+    for (let col = 0; col < 3; col++) {
+      let isColumnOWinner = true;
+      let isColumnXWinner = true;
+
+      for (let row = 0; row < 3; row++) {
+        if (gameMap[row][col] !== "o") {
+          isColumnOWinner = false;
+        }
+        if (gameMap[row][col] !== "x") {
+          isColumnXWinner = false;
+        }
+      }
+      if (isColumnOWinner) {
+        gameWon("o");
+        break;
+      }
+      if (isColumnXWinner) {
+        gameWon("x");
+        break;
+      }
+    }
+    //End column result
+
+    // Diagonal result
+    let isDiagonal1oWinnning = true;
+    let isDiagonal1xWinnning = true;
+    let isDiagonal2oWinnning = true;
+    let isDiagonal2xWinnning = true;
+
+    for (let i = 0; i < 3; i++) {
+      if (gameMap[i][i] !== "o") {
+        isDiagonal1oWinnning = false;
+      }
+      if (gameMap[i][i] !== "x") {
+        isDiagonal1xWinnning = false;
+      }
+
+      if (gameMap[i][2 - i] !== "o") {
+        isDiagonal2oWinnning = false;
+      }
+      if (gameMap[i][2 - i] !== "x") {
+        isDiagonal2xWinnning = false;
+      }
+    }
+    if (isDiagonal1oWinnning || isDiagonal2oWinnning) {
+      gameWon("o");
+    }
+    if (isDiagonal1xWinnning || isDiagonal2xWinnning) {
+      gameWon("x");
+    }
+    //End diagonal result
+  };
+  const resetGame = () => {
+    setGameMap([
+      ["", "", ""], //first row
+      ["", "", ""], //second row
+      ["", "", ""], //third row
+    ]);
+    setCurrentTurn("o");
+  };
+  const gameWon = (player) => {
+    Alert.alert('Woohoo!',`Player '${player}' won`, [
+      {
+        text: "Reset game",
+        onPress: resetGame
+      },
+    ]);
   };
 
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <ImageBackground source={bg} style={styles.background} resizeMode="contain">
+      <ImageBackground
+        source={bg}
+        style={styles.background}
+        resizeMode="contain"
+      >
         <View style={styles.map}>
           {gameMap.map((row, rowIndex) => (
-            <View style={styles.row}>
+            <View key={`row-${rowIndex}`} style={styles.row}>
               {row.map((cell, columnIndex) => (
                 <Pressable
+                  key={`row-${rowIndex}-col-${columnIndex}`}
                   onPress={() => onPress(rowIndex, columnIndex)}
                   style={styles.cell}
                 >
